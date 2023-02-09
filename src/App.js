113f6651862746de311aadf2c753f8b58c6ef32e
tsotne123 <tsotnedarjania1997@gmail.com>
2023-02-09 21:28:04 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AudioRecorder } from "react-audio-voice-recorder";
+import TodoList from "./TodoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <TodoList/>
+    <AudioRecorder onRecordingComplete={<button>Clear Complate</button>} />
+    <input type="text" />
+    <input type="text" />
+    <button>Add Todo</button>
+    <button>Clear Complate</button>
+    <div> left to do</div>
+
+    </>
+  )
 }
 
 export default App;
